b198d1a1400f4112a866d26aca2e7ec1230917b3
Hamed omari <118768898+Hamedomari@users.noreply.github.com>
2023-11-12 21:14:43 +0100

    v1

---
+++ src/test/java/myapp/tests/Class.java
@@ -0,0 +1,15 @@
+package myapp.tests;
+
+import myapp.utilities.Driver;
+import org.testng.annotations.Test;
+
+public class Class {
+    @Test
+    public void test(){
+        Driver.getDriver().get("https://www.google.com");
+    }
+    @Test
+    public void test2(){
+
+    }
+}
