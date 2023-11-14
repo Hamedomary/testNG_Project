481e064c2e9519b3bf1079bf8dd9bd06f0eaf7b0
meliksahin05 <meliksahin05@gmail.com>
2023-11-14 23:22:53 +0200

    m

---
+++ src/test/java/myapp/tests/US_05_TC01.java
@@ -0,0 +1,26 @@
+package myapp.tests;
+
+public class US_05_TC01 {
+
+
+
+//    User goes to the webpage
+
+//    User logs in with a registered customer account
+//    Verify (My account) link is visible
+//    Click on the MY ACCOUNT
+//    Verify (Account Details) link is visible
+//    Click on the ACCOUNT DETAILS
+//    User click in the First Name box and type a name
+//    User click on the LAST NAME and type a name
+//    User click on the Display Name and remove existing Display Name then  type new Name
+//    User click on the EMAIL ADDRESS and remove existing email address then type new valid email
+//    User click on the Biography section and type lorem
+//    User click on the current password and type the current one
+//    User click on the new password and type the new password
+//    User click on the confirm password and type the password you write for new password
+//    Verify the save changes button
+//    User click on "SAVE CHANGES" BUTTON
+//    Verify the message "Account details changed successfully
+
+}
