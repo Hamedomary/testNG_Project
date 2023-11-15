127112532306962acc6b9fa668dc24d4d7a5f032
meliksahin05 <meliksahin05@gmail.com>
2023-11-15 03:42:51 +0200

    Us_005_tc_001

--- src/test/java/myapp/tests/US_05_TC01.java
+++ src/test/java/myapp/tests/US_05_TC01.java
@@ -1,13 +1,43 @@
 package myapp.tests;
 
+import myapp.pages.melik.Allovercommerce_homepage;
+import myapp.pages.melik.Allovercommerce_loginpage;
+import myapp.utilities.ConfigReader;
+import myapp.utilities.Driver;
+import myapp.utilities.JSUtils;
+import myapp.utilities.WaitUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import static myapp.utilities.JSUtils.flashElement;
+
+
+@Listeners(myapp.utilities.ListenerUtils.class)
 public class US_05_TC01 {
 
-
+    @Test
+    public void US_05_TC01() throws InterruptedException {
 
 //    User goes to the webpage
-
+        Driver.getDriver().get(ConfigReader.getProperty("allovercommerce_url"));
 //    User logs in with a registered customer account
+        Allovercommerce_homepage allovercommerce_homepage = new Allovercommerce_homepage();
+        allovercommerce_homepage.signIn.click();
+        Allovercommerce_loginpage allovercommerce_loginpage = new Allovercommerce_loginpage();
+        allovercommerce_loginpage.usurname.sendKeys(ConfigReader.getProperty("allovercommerce_username"));
+        allovercommerce_loginpage.password.sendKeys(ConfigReader.getProperty("allovercommerce_password"));
+        flashElement(allovercommerce_loginpage.signInButton);
+        allovercommerce_loginpage.signInButton.click();
+//        Verify that user on the home page
+        WaitUtils.waitForVisibility(By.xpath("//a[.='Sign Out']"),7); //if signout element visible means that user on the page,
+
 //    Verify (My account) link is visible
+        JSUtils.JSscrollAllTheWayDown();
+        Assert.assertTrue(allovercommerce_homepage.MyAccount.isDisplayed());
+
 //    Click on the MY ACCOUNT
 //    Verify (Account Details) link is visible
 //    Click on the ACCOUNT DETAILS
@@ -22,5 +52,11 @@ public class US_05_TC01 {
 //    Verify the save changes button
 //    User click on "SAVE CHANGES" BUTTON
 //    Verify the message "Account details changed successfully
+//    Close the driver
+//      Driver.closeDriver();
+
+
+
+    }
 
 }
